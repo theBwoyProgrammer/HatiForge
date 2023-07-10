9dccb7314f6a018b7a096ba57247d1702d1046af
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-07-10 15:23:55 +0300

    added ham menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,24 @@
 // pages/index.js
 import React from 'react';
 import styles from '../styles/styles.module.css';
+import HamburgerMenu, { Links } from '../components/hamburger.js'
 
 const Home = () => {
   return (
     <div className={styles.welcome_container}>
-      <nav className='nav-sec'>
+      {/* <nav className={styles.nav_sec}>
         <a href="#">Home</a>
         <a href="#">About</a>
         <a href="#">Services</a>
         <a href="#">Contact</a>
-      </nav>
+      </nav> */}
+
+       <div className='max-w-full h-12 flex justify-start items-center bg-black mb-4 text-white rounded-md '>
+            <div className='flex md:hidden'><HamburgerMenu /></div>
+            <div className='hidden md:flex'>
+                <Links />
+            </div>
+        </div>
       <div className={styles.container}>
         <h1 className='welcome-h1'>We are HatiForge</h1>
         <div className={styles.slogan}>
